c595d5a8b65a12082fe4ee097bbb157065094c0e
oakkas <oakkas84@gmail.com>
2020-07-29 15:45:13 -0600

    Added instance

--- main.tf
+++ main.tf
@@ -1,11 +1,31 @@
 provider "google" {
   credentials = file("terraform.json")
 
-  project = "	academic-being-283513"
+  project = "academic-being-283513"
   region = "us-central1"
   zone = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance"{
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+    tags = ["web", "dev"]
+
+    boot_disk{
+      initialize_params{
+        image = "ubuntu-os-cloud/ubuntu-1604-xenial-v20200722"
+      }
+    }
+    
+
+
+    network_interface{
+      network = google_compute_network.vpc_network.name
+      access_config{}
+    }
+
 }
