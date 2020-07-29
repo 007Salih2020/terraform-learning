a48783e36f13c3ca4c71102edc65353242aaefd4
oakkas <oakkas84@gmail.com>
2020-07-29 15:03:52 -0600

    network-create

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "google" {
   credentials = file("terraform.json")
+
+  project = "	academic-being-283513"
+  region = "us-central1"
+  zone = "us-central1-c"
+}
+
+resouce "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
