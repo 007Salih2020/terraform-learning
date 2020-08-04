82f0ad0ca5a45959caee9c85ca4b606bc38213ca
oakkas <oakkas84@gmail.com>
2020-08-04 14:38:36 -0600

    added resources

--- main.tf
+++ main.tf
@@ -2,33 +2,40 @@ provider "google" {
   credentials = file("terraform.json")
 
   project = "academic-being-283513"
-  region = "us-central1"
-  zone = "us-central1-c"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-resource "google_compute_instance" "vm_instance"{
-    name = "terraform-instance"
-    machine_type = "f1-micro"
-    tags = ["web", "dev"]
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  depends_on   = [google_storage_bucket.example_bucket]
 
-    boot_disk{
-      initialize_params{
-        image = "ubuntu-os-cloud/ubuntu-1604-xenial-v20200722"
-      }
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1604-xenial-v20200722"
     }
-    
-    network_interface{
-      network = google_compute_network.vpc_network.name
-      access_config{
-        nat_ip = google_compute_address.vm_static_ip.address
-      }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
+  }
 }
 
-resource "google_compute_address" "vm_static_ip"{
+resource "google_compute_address" "vm_static_ip" {
   name = "terraform-static-ip"
+}
+
+# storage creation
+resource "google_storage_bucket" "example_bucket" {
+  name     = "my_example_bucket_oguz"
+  location = "US"
 }
